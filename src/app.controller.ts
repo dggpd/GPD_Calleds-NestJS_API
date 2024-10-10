fc9b34f38405f06e635ae924b07912b19a7df1fb
biamaxine <2023005718@ifam.edu.br>
2024-10-10 09:51:45 -0400

    feat: ADD Swagger and Morgan

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { Default } from './shared/interfaces/default.interface';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('API Data')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as morgan from 'morgan';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('GPD Calleds')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(morgan('dev'));
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
